665a555aecea14e9928c2998431fa9796b063a84
excitingmvr <excitingmvr>
2024-04-16 15:08:54 +0900

    first

---
+++ abc/src/test/java/com/septemberfw/AbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.septemberfw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
